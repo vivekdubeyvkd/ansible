d2164e9010d1f8bd3728862f184e4db02529d7b1
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2020-03-18 15:47:52 +0530

    Update run_ansible_roles.sh

--- run_ansible_roles.sh
+++ run_ansible_roles.sh
@@ -1,3 +1,4 @@
 ansible-playbook -i hosts gradle.yml -K
 ansible-playbook -i hosts elasticsearch.yml -K
 ansible-playbook -i hosts rbenv.yml -K
+ansible-playbook -i hosts aws-provision-ec2-machine.yml --extra-vars "tag_name=newec2machine"
