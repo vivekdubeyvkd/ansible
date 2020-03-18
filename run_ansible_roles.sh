13b36cdf33971dcad127a76deae50c789100d6c1
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2020-03-18 16:00:46 +0530

    Update run_ansible_roles.sh

--- run_ansible_roles.sh
+++ run_ansible_roles.sh
@@ -2,4 +2,4 @@ ansible-playbook -i hosts gradle.yml -K
 ansible-playbook -i hosts elasticsearch.yml -K
 ansible-playbook -i hosts rbenv.yml -K
 ansible-playbook -i hosts aws-provision-ec2-machine.yml --extra-vars "tag_name=newec2machine"
-ansible-playbook -i hosts create-ami.yml --extra-vars "source_instance_id=i-111111dhdhdhdhhd new_ami_name=newami  base_ami=ami-090dfffbdjdjjdj1"
+ansible-playbook -i hosts create-aws-ami.yml --extra-vars "source_instance_id=i-111111dhdhdhdhhd new_ami_name=newami  base_ami=ami-090dfffbdjdjjdj1"
